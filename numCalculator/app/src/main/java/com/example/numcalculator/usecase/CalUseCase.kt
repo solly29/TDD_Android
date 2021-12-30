1d029171f6207d93fe105bcc65bbe1612adc6d19
solly29 <cksdud1311@naver.com>
2021-12-30 23:14:45 +0900

    2021-12-30 설계 변경
    설계 변경으로 인한 다시 시작..
    CalUseCase.kt 테스트 검증

--- numCalculator/app/src/main/java/com/example/numcalculator/usecase/CalUseCase.kt
+++ numCalculator/app/src/main/java/com/example/numcalculator/usecase/CalUseCase.kt
@@ -1,40 +1,11 @@
 package com.example.numcalculator.usecase
 
-import com.example.numcalculator.CalToken
-import com.example.numcalculator.calResult
-import kotlin.math.floor
-import kotlin.math.roundToInt
+import com.example.numcalculator.model.CalNumberData
 
-class CalUseCase: UseCase<String, Number>() {
-    override fun run(parm: String): Number {
-
-        val tokenList: List<CalToken> = parm.split(" ").map {
-            CalToken(it)
+class CalUseCase: UseCase<CalNumberData, Number>() {
+    override fun run(parm: CalNumberData): Number {
+        return parm.run {
+            operation.calculate(number1, number2)
         }
-
-        return tokenList.calResult()
-    }
-}
-
-enum class Operation(val operation: String, val calculate: (Double, Double) -> Double) {
-    PLUS("+", {num1, num2 -> num1 + num2}),
-    MINUS("-", {num1, num2 -> num1 - num2}),
-    PRODUCT("*", {num1, num2 -> num1 * num2}),
-    DIVIDE("/", {num1, num2 -> num1 / num2}),
-    REMAINDER("%", {num1, num2 -> num1 % num2});
-
-    companion object {
-        fun find(operation: String): Operation {
-            return values().find { it.operation == operation }
-                ?: throw NullPointerException("$operation 연산자는 없습니다.")
-        }
-    }
-}
-
-fun Double.toRound(): Number {
-    return if(mod(roundToInt().toDouble()) == 0.0) {
-        roundToInt()
-    } else {
-        floor(this * 10000) / 10000
     }
 }
